3a190bb758d438cd7b65b7fc18574016b24bd4d2
D4mmmi <Woahitzdami@gmail.com>
2025-04-19 19:14:19 -0400

    fix filtered

--- my-app/src/FilteredList.jsx
+++ my-app/src/FilteredList.jsx
@@ -1,54 +1,64 @@
 import React, { Component } from 'react';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import List from './List';
 
 class FilteredList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      type: "all"
+    };
+  }
 
-        // Add a new key/value pair in the state to keep track of type
-        this.state = {
-            search: "",
-            type: "all"
-        };
-    }
+  // Handles the search bar input
+  onSearch = (event) => {
+    this.setState({ search: event.target.value.trim().toLowerCase() });
+  }
 
-    //  Update search string in state
-    onSearch = (event) => {
-        this.setState({ search: event.target.value.trim().toLowerCase() });
-    }
+  // Handles dropdown selection
+  handleSelect = (eventKey, event) => {
+    this.setState({ type: eventKey });
+  }
 
-    //Dropdown selection handler
-    onFilter = (eventKey, event) => {
-        this.setState({ type: eventKey });
-    }
+  // Filters each item based on search and type
+  filterItem = (item) => {
+    const matchesSearch = item.name.toLowerCase().includes(this.state.search);
+    const matchesType = this.state.type === "all" || item.type === this.state.type;
+    return matchesSearch && matchesType;
+  }
 
-    // Filter by name and type
-    filterItem = (item) => {
-        const matchesSearch = item.name.toLowerCase().includes(this.state.search);
-        const matchesType = this.state.type === "all" || item.type.toLowerCase() === this.state.type;
-        return matchesSearch && matchesType;
-    }
+  render() {
+    return (
+      <div className="filter-list">
+        <h1>Produce Search</h1>
 
-    render() {
-        return (
-            <div className="filter-list">
-                <h1>Produce Search</h1>
+        {/* Dropdown to filter by type */}
+        <Dropdown onSelect={this.handleSelect}>
+          <Dropdown.Toggle variant="primary" id="dropdown-basic">
+            {this.state.type.charAt(0).toUpperCase() + this.state.type.slice(1)}
+          </Dropdown.Toggle>
 
-                {/* DropdownButton with all filters */}
-                <DropdownButton id="typeDropdown" title={`Type: ${this.state.type}`} onSelect={this.onFilter}>
-                    <MenuItem eventKey="all">All</MenuItem>
-                    <MenuItem eventKey="fruit">Fruit</MenuItem>
-                    <MenuItem eventKey="vegetable">Vegetable</MenuItem>
-                </DropdownButton>
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="all">All</Dropdown.Item>
+            <Dropdown.Item eventKey="fruit">Fruit</Dropdown.Item>
+            <Dropdown.Item eventKey="vegetable">Vegetable</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
 
-                <input type="text" placeholder="Search" onChange={this.onSearch} />
+        {/* Search input */}
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={this.onSearch}
+          style={{ marginTop: "10px", padding: "5px", width: "100%" }}
+        />
 
-                {/ Filtered List output */}
-                <List items={this.props.items.filter(this.filterItem)} />
-            </div>
-        );
-    }
+        {/* Filtered list of items */}
+        <List items={this.props.items.filter(this.filterItem)} />
+      </div>
+    );
+  }
 }
 
 export default FilteredList;
